566f1468e0afe3d898aaf2552bac2c6d0c296179
Bind Vivek Kumar Bind <bind74573@gmail.com>
2025-09-23 15:21:09 +0530

    added arrowfunction

--- javascript/sum.js
+++ javascript/sum.js
@@ -1,12 +1,22 @@
-function sum(a, b) {
-    console.log(a + b);
-}
+// function sum(a, b) {
+//     console.log(a + b);
+// }
 
-sum(5, 7);
+// sum(5, 7);
 
 
-function sub(a, b) {
-    console.log(a - b);
-}
+// function sub(a, b) {
+//     console.log(a - b);
+// }
 
-sub(9, 7);
+// sub(9, 7);
+
+
+
+       var a=70;
+        var b=334;
+       var v=a+b;
+        console.log(v);
+
+
+
